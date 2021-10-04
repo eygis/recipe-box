0389d6c6fd4aae47d769e14142b7f432cfac263d
eygis <86146074+eygis@users.noreply.github.com>
2021-10-04 17:32:23 +0900

    initial development commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+    <h1 id="title">Quick and Easy Recipes Box</h1>
+    <div id="recipeDisplay">  
+      test
+    </div>
+    <div id="recipeInput">
+      test
+    </div>
     </div>
   );
+  }
 }
 
 export default App;
