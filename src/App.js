24e77277a8d0008e3a0f8926f7ab65538e82003b
eygis <86146074+eygis@users.noreply.github.com>
2021-10-05 20:01:26 +0900

    day 2

--- src/App.js
+++ src/App.js
@@ -4,20 +4,38 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      meal: "ramen",
+      ingredients: "noodles",
+      notes: "it's good" 
+    }
+    
   }
   render() {
+    
   return (
     <div id="wrapper">
     <h1 id="title">Quick and Easy Recipes Box</h1>
     <div id="recipeDisplay">  
-      test
+    <Tile meal={this.state.meal} ingredients={this.state.ingredients} notes={this.state.notes} />
     </div>
     <div id="recipeInput">
-      test
+      <button>Add New Recipe</button>
     </div>
     </div>
   );
+  }
+}
+
+class Tile extends React.Component {
+  render () {
+    const { meal, ingredients, notes} = this.props;
+    return (
+      <div className="tile">
+       <div className="tileTop"><h2>{meal}</h2></div>
+       <div className="content"><p>{ingredients}</p><span>{notes}</span></div> 
+      </div>
+    )
   }
 }
 
