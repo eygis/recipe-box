2148ce85aa774199355b2abef342134bc9b1bb37
eygis <86146074+eygis@users.noreply.github.com>
2021-10-06 18:55:30 +0900

    10/6

--- src/App.js
+++ src/App.js
@@ -5,29 +5,35 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      meal: "ramen",
-      ingredients: "noodles",
-      notes: "it's good" 
+      meal: "",
+      ingredients: "select a meal to see the recipe!",
+      notes: "" 
     }
+    
     
   }
   render() {
-    
+    let recipes = ["test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9"];
   return (
     <div id="wrapper">
     <h1 id="title">Quick and Easy Recipes Box</h1>
     <div id="recipeDisplay">  
-    <Tile meal={this.state.meal} ingredients={this.state.ingredients} notes={this.state.notes} />
+    <Recipe meal={this.state.meal} ingredients={this.state.ingredients} notes={this.state.notes} />
     </div>
-    <div id="recipeInput">
-      <button>Add New Recipe</button>
+    <div id="catalog">
+      <ul>
+      {recipes.map((i) => <li key={i}>{i}</li>)}
+      </ul>
     </div>
+    
+      <button id="newRecipeButton">Add New Recipe</button>
+    
     </div>
   );
   }
 }
 
-class Tile extends React.Component {
+class Recipe extends React.Component {
   render () {
     const { meal, ingredients, notes} = this.props;
     return (
